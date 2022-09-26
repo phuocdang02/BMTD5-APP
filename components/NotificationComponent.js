32f8fb4a5611f72f5949463d958e7a5c7766c722
PhuocDeng <haphuocdang@gmail.com>
2022-09-26 19:14:37 +0700

    change a readme.md

--- components/NotificationComponent.js
+++ components/NotificationComponent.js
@@ -16,7 +16,7 @@ class Notification extends Component {
     return (
       <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
         <Card>
-          <Card.Title>Contact Information</Card.Title>
+          <Card.Title>Notification Center</Card.Title>
           <Card.Divider />
           <Text style={{ margin: 10 }}>Email: {this.state.email}</Text>
           <Button
@@ -29,6 +29,7 @@ class Notification extends Component {
       </Animatable.View>
     );
   }
+
   componentDidMount() {
     const dbRef = ref(getDatabase());
     onValue(child(dbRef, "contact/"), (snapshot) => {
@@ -38,6 +39,8 @@ class Notification extends Component {
       });
     });
   }
+
+  /* Function Auto Send Mail when "borrowdate" || "returndate" is comming */
   composeMailForBorrowDate() {
     MailComposer.composeAsync({
       recipients: ["haphuocdang@gmail.com"],
@@ -51,6 +54,14 @@ class Notification extends Component {
       subject: "RETURN Reminder [7]days",
       body: "Class {name} return date is comming, please prepare available book",
     });
+  }
+
+  autocomposeMail(){
+    if (this.state.borrowdate == 8 || this.state.returndate == 8) {
+      
+    } else {
+      
+    }
   }
 }
 export default Notification;
--- components/ReadMe.md
+++ components/ReadMe.md
@@ -11,3 +11,9 @@ Components Flow
         <!-- ClassScreen - list Class imported -->
 
         <!-- TeacherScreen - list Teacher imported -->
+
+
+        <!-- If want to send gmail automatically -->
+            <!-- 1. Get value of borrow day || return day -->
+            <!-- 2. Take the value to GGsheet ???  -->
+            <!-- 3. Using AutoEmails extension -->
